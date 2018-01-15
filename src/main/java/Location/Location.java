9817119a0f0e726dd1f95086d0316d56cf867e73
Douglas Sangster <d.sangster@hotmail.co.uk>
2018-01-15 00:15:52 +0000

    removed concept of occupancy from location class - tracked by bookings instead

--- src/main/java/Location/Location.java
+++ src/main/java/Location/Location.java
@@ -14,20 +14,11 @@ public class Location {
     public Location(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
-        this.occupants = new ArrayList<>();
     }
 
     public ArrayList<Guest> getOccupants() {
         ArrayList<Guest> copyOccupants = this.occupants;
         return copyOccupants;
-    }
-
-    public void receiveGuests(Guest... guests) {
-        this.occupants.addAll(Arrays.asList(guests));
-    }
-
-    public void releaseGuests(Guest... guests) {
-        this.occupants.removeAll(Arrays.asList(guests));
     }
 
     public int getCapacity() {
--- src/test/java/LocationTest/LocationTest.java
+++ src/test/java/LocationTest/LocationTest.java
@@ -1,7 +1,6 @@
 package LocationTest;
 
 import Location.Location;
-import Person.Guest;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,41 +9,15 @@ import static org.junit.Assert.assertEquals;
 public class LocationTest {
 
     private Location room;
-    private Guest guest1;
-    private Guest guest2;
-    private Guest guest3;
 
     @Before
     public void before(){
         room = new Location("The Room", 2);
-        guest1 = new Guest("Flava","Flavia", "Maury", 20);
-        guest2 = new Guest("DeDe","Duncan", "Jacques", 40);
-        guest3 = new Guest("Lala","Leira", "Woodrow", 80);
-    }
-
-    @Test
-    public void checkOccupancyDefaultsToEmpty(){
-        assertEquals(true, room.getOccupants().isEmpty());
     }
 
     @Test
     public void canGetRoomCapacity(){
         assertEquals(2, room.getCapacity());
-    }
-
-    @Test
-    public void canReceiveGuests(){
-        room.receiveGuests(guest1, guest2);
-        assertEquals(2, room.getOccupants().size());
-        assertEquals(true, room.getOccupants().contains(guest2));
-    }
-
-    @Test
-    public void canReleaseGuests(){
-        room.receiveGuests(guest1, guest3, guest2);
-        room.releaseGuests(guest2, guest1);
-        assertEquals(1, room.getOccupants().size());
-        assertEquals(true, room.getOccupants().contains(guest3));
     }
 
     @Test
