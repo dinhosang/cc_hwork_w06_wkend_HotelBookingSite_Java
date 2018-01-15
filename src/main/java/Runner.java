714ceaa289a86614f3b12ad7013433d002b53f29
Douglas Sangster <d.sangster@hotmail.co.uk>
2018-01-15 04:55:14 +0000

    tidied up runner file

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -5,10 +5,9 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        while(true){
-            TravelInCode site = new TravelInCode();
-            RunnerHelper helper = new RunnerHelper(site);
-            helper.welcomeScreen();
-        }
+        TravelInCode site = new TravelInCode();
+        RunnerHelper helper = new RunnerHelper(site);
+        helper.welcomeScreen();
+
     }
 }
